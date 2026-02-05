052471563141c0f4c801e5815fdecfbec8696ade
Rylan Graham <rylangraham02@gmail.com>
2026-02-05 19:35:52 +0100

    Test Fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Pre-Demonstration';
+    return 'Hello World!';
   }
 }
